6485b80e5e11f1b6d609f9f82c739f8c8cabbd64
UezonoKou <36868799+uezonokou@users.noreply.github.com>
2019-03-12 11:58:08 +0900

    過去の記録への遷移追加

---
+++ app/src/main/java/com/example/fuelmemory/HistoryActivity.java
@@ -0,0 +1,23 @@
+package com.example.fuelmemory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HistoryActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        Button back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                public void onClick (View v){
+                    finish();
+                }
+            }
+        });
+    }
+}
