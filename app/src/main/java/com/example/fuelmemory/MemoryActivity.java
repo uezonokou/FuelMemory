dd6692faf426b686493ebe6b9dfba3dfe324243c
UezonoKou <36868799+uezonokou@users.noreply.github.com>
2019-03-13 10:50:49 +0900

    入力画面小改良・時刻自動入力

--- app/src/main/java/com/example/fuelmemory/MemoryActivity.java
+++ app/src/main/java/com/example/fuelmemory/MemoryActivity.java
@@ -2,10 +2,17 @@ package com.example.fuelmemory;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class MemoryActivity extends Activity {
+
+    public EditText daytime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +20,19 @@ public class MemoryActivity extends Activity {
 
         setContentView(R.layout.acivity_memory);
 
+        daytime=findViewById(R.id.daytime_me);
+
+        Date data=new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy'年'MM'月'dd'日' kk'時'mm'分'ss'秒'");
+
+        daytime.setText(sdf.format(data));
+
         Button sendButton = findViewById(R.id.sendMemory);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String sendDaytime=daytime.getText().toString();
                 finish();
             }
         });
