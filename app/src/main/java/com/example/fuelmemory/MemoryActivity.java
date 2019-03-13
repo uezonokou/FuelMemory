34abf94cf96198a90856cb8b39af6fc8c6dbf729
UezonoKou <36868799+uezonokou@users.noreply.github.com>
2019-03-13 11:54:28 +0900

    計算・トースト出力 画面見やすさ向上

--- app/src/main/java/com/example/fuelmemory/MemoryActivity.java
+++ app/src/main/java/com/example/fuelmemory/MemoryActivity.java
@@ -6,12 +6,17 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class MemoryActivity extends Activity {
 
     public EditText daytime;
+    public EditText distance;
+    public EditText fuel_amount;
+    public EditText Money;
 
 
     @Override
@@ -28,11 +33,33 @@ public class MemoryActivity extends Activity {
 
         daytime.setText(sdf.format(data));
 
+        distance=findViewById(R.id.distance_m);
+
+        fuel_amount=findViewById(R.id.fuel_amount_m);
+
+        Money=findViewById(R.id.Money_m);
+
+
+
         Button sendButton = findViewById(R.id.sendMemory);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String sendDaytime=daytime.getText().toString();
+                String stdistance =distance.getText().toString();
+                double dis = Double.valueOf(stdistance);
+                String stfuel = fuel_amount.getText().toString();
+                double fuel_am = Double.valueOf(stfuel);
+                double ans = dis / fuel_am;
+                String stMoney = Money.getText().toString();
+                double money_d = Double.valueOf(stMoney);
+                double L = money_d / fuel_am;
+
+                String toastmsg= "燃費は"+ans+"km/lです。\n1L当たり"+L+"円です。";
+
+                Toast.makeText(MemoryActivity.this,toastmsg,Toast.LENGTH_LONG).show();
+
+
                 finish();
             }
         });
