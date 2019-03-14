5e6cfb7faeefff3bdba1ec437ca7490ef075e1a2
UezonoKou <36868799+uezonokou@users.noreply.github.com>
2019-03-14 11:58:00 +0900

    初期設定editText連携

--- app/src/main/java/com/example/fuelmemory/StartActivity.java
+++ app/src/main/java/com/example/fuelmemory/StartActivity.java
@@ -5,13 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class StartActivity extends Activity {
+
+    public EditText Carname;
+    public EditText startdis;
+    public EditText Memo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Carname=findViewById(R.id.CarName);
+        startdis=findViewById(R.id.Start_Distance);
+        Memo=findViewById(R.id.memo);
+
+
 
         Button ok = findViewById(R.id.OK);
         ok.setOnClickListener(new View.OnClickListener() {
